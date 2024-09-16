13f17b3636e7ecd1c8e84b8a24d9137e3d366ade
Burak Sekili <buraksekili@gmail.com>
2024-09-16 19:13:15 +0300

    rayon
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- src/thread_pool/rayon.rs
+++ src/thread_pool/rayon.rs
@@ -1,19 +1,21 @@
 use super::ThreadPool;
+use crate::{KvsError, Result};
 
-pub struct RayonThreadPool {}
+pub struct RayonThreadPool(rayon::ThreadPool);
 
 impl ThreadPool for RayonThreadPool {
-    fn new(threads: u32) -> crate::Result<Self>
-    where
-        Self: Sized,
-    {
-        todo!()
+    fn new(threads: u32) -> Result<Self> {
+        let pool = rayon::ThreadPoolBuilder::new()
+            .num_threads(threads as usize)
+            .build()
+            .map_err(|e| KvsError::Pooling)?;
+        Ok(RayonThreadPool(pool))
     }
 
     fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        todo!()
+        self.0.spawn(job)
     }
 }
